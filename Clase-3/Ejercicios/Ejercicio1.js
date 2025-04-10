009f56ad7c94d19d36ec59727abd52910862fb63
alonsojmateo <alonso.jmateo@gmail.com>
2025-04-10 19:12:42 -0300

    add ej 1

--- Clase-3/Ejercicios/Ejercicio1.js
+++ Clase-3/Ejercicios/Ejercicio1.js
@@ -15,8 +15,41 @@
 // id, name, height, weight, 
 // sprites.other.showdown.front_default
 
-function obtenerPokemon(Ids){
+const url = "https://pokeapi.co/api/v2/pokemon"
 
+async function obtenerPokemon(Ids){
+    const data = await fetch(`${url}?limit=151&offset=0`)
+        .then(res => res.json())
+        .then((data) => data.results.filter((_, index) => Ids.includes(index + 1)))
+        .catch(err => {
+            console.log("Error al obtener 150 pokemones")
+            return []
+        })
+
+    const promesas = data.map((pokemon, index) => {
+        return fetch(pokemon.url)
+            .then(res => res.json())
+            .catch(err => {
+                console.log("Error al obtener pokemon: " + index);
+                return null
+            } )
+    })
+
+    const rawPokemones = await Promise.all(promesas)
+
+    const respuesta = {}
+
+    rawPokemones
+        .filter(poke => poke !== null)
+        .map((pokemon) => {
+            respuesta[pokemon.name] = {
+                id: pokemon.id,
+                name: pokemon.name,
+                height: pokemon.height
+            }
+    })
+
+    return respuesta;
     // fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
     // Filtren todos esos y quedense con los que tengan los ids correspondientes
 
@@ -31,6 +64,9 @@ function obtenerPokemon(Ids){
     //     // }
 }
 
-let ids = [3, 20, 40, 80, 148, 151]
+let ids = [4, 3, 20, 40, 80, 148, 151]
 
 obtenerPokemon(ids)
+    .then(resultado => {
+        console.log("Resultado: ", resultado);
+    })
